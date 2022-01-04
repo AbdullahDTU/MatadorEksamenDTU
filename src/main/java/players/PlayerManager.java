e405345239c03981fa86bbbd18eb79767baff2fd
s203952 <91071190+s203952@users.noreply.github.com>
2022-01-04 12:29:57 +0100

    Made a playerList and a createPlayer method

--- src/main/java/players/PlayerManager.java
+++ src/main/java/players/PlayerManager.java
@@ -1,5 +1,35 @@
 package players;
 
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+import mainGame.Setup;
+
 public class PlayerManager {
     //Playercount and Players list
+
+    int i = 1;
+    public void playersArrayList(Player player) {
+        i++;
+        switch (i) {
+            case 1:
+                Player player1 = player;
+                break;
+            case 2:
+                Player player2 = player;
+                break;
+            case 3:
+                Player player3 = player;
+                break;
+            case 4:
+                Player player4 = player;
+                break;
+            default:
+        }
+    }
+
+    public void createPlayer(String name, int balance, GUI_Car car) {
+        GUI_Player GUIPlayer = new GUI_Player(name, balance,car);
+        Player player = new Player(Setup.START_FIELD,GUIPlayer);
+        playersArrayList(Player player);
+    }
 }
