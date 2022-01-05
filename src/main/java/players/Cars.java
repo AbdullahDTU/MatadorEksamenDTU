c07ea706b3e78d93194fb372aacdf7db91ed4cf3
Abdu <Abdu0901@ltkskole.dk>
2022-01-05 12:08:18 +0100

    Added Car Class with 4 type of cars and patterns

--- src/main/java/players/Cars.java
+++ src/main/java/players/Cars.java
@@ -5,44 +5,37 @@ import gui_fields.GUI_Car;
 import java.awt.*;
 
 public class Cars {
-    private GUI_Car[] cars;
 
-    public Cars() {
-            createCars();
-        }
+    private static GUI_Car[] cars = new GUI_Car[]{
+            //Regular car
+            new GUI_Car(Color.GREEN,
+                    Color.BLACK,
+                    GUI_Car.Type.CAR,
+                    GUI_Car.Pattern.HORIZONTAL_GRADIANT),
 
-        /**
-         * Creates all the cars necessary to begin play the game
-         */
-        private void createCars() {
-            cars = new GUI_Car[]{
-                    //the car (bilen)
-                    new GUI_Car(new Color(89, 144, 95),
-                            new Color(143, 182, 89),
-                            GUI_Car.Type.CAR,
-                            GUI_Car.Pattern.HORIZONTAL_GRADIANT),
+            //Ufo
+            new GUI_Car(Color.GREEN,
+                    Color.RED,
+                    GUI_Car.Type.UFO,
+                    GUI_Car.Pattern.DOTTED),
 
-                    //the ufo (skibet)
-                    new GUI_Car(new Color(126, 159, 199),
-                            new Color(112, 112, 153),
-                            GUI_Car.Type.UFO,
-                            GUI_Car.Pattern.DOTTED),
+            //Race Car
+            new GUI_Car(Color.BLACK,
+                    Color.YELLOW,
+                    GUI_Car.Type.RACECAR,
+                    GUI_Car.Pattern.ZEBRA),
 
-                    //the racecar (katten)
-                    new GUI_Car(new Color(227, 153, 81),
-                            new Color(162, 89, 73),
-                            GUI_Car.Type.RACECAR,
-                            GUI_Car.Pattern.ZEBRA),
+            //Tractor
+            new GUI_Car(Color.BLUE,
+                    Color.BLACK,
+                    GUI_Car.Type.TRACTOR,
+                    GUI_Car.Pattern.HORIZONTAL_GRADIANT),
 
-                    //the tractor (hunden)
-                    new GUI_Car(new Color(57, 69, 68),
-                            new Color(163, 170, 171),
-                            GUI_Car.Type.TRACTOR,
-                            GUI_Car.Pattern.HORIZONTAL_LINE),
-            };
-        }
-        public GUI_Car[] getCars() {
-            return cars;
-        }
+    };
+
+    public static GUI_Car[] getCars() {
+        return cars;
+    }
+
+
 }
-
