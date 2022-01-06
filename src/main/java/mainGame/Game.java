a2453593f0d641b6a9032e90fbf447a77bcd9cb5
Abdu <Abdu0901@ltkskole.dk>
2022-01-06 10:14:30 +0100

    Added initiliazation of game and main

--- src/main/java/mainGame/Game.java
+++ src/main/java/mainGame/Game.java
@@ -1,11 +1,14 @@
 package mainGame;
 
+import players.PlayerManager;
+
 public class Game {
 
-    public void game() {
-        setPlayerBalance(1, START_MONEY);
-        setPlayerBalance(2, START_MONEY);
-        setPlayerBalance(3, START_MONEY);
-        setPlayerBalance(4, START_MONEY);
+        public void initializeGame () {
+            Setup setup = new Setup();
+            setup.setupPlayers();
+            PlayerManager playerManager = new PlayerManager();
+            System.out.println(playerManager.getPlayerName(2));
+            System.out.println(playerManager.getPlayerBalance(2));
+        }
     }
-}
