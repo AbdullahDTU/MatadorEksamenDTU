3e7eca337b7880c256384c8c09a222dc52da8d58
Betim <s216250@student.dtu.dk>
2022-01-12 12:52:44 +0100

    CardTemplate index updated and number of cards

--- src/main/java/chanceCards/CardTemplate.java
+++ src/main/java/chanceCards/CardTemplate.java
@@ -42,177 +42,182 @@ public class CardTemplate {
     CardTemplate propertyTaxes = new CardTemplate(
             "Property Taxes",
             "Property taxes have risen. The extra costs are kr. 800 per house, kr. 2.300 per hotel",
-            2);
+            3);
 
     CardTemplate carWash = new CardTemplate(
             "Car Wash",
             "Pay for car wash and lubrication kr. 300",
-            2);
+            4);
 
     CardTemplate deliveryBox = new CardTemplate(
             "Delivery Box",
             "Pay kr. 200 for delivery of 2 boxes of beer",
-            2);
+            5);
 
     CardTemplate carRepair = new CardTemplate(
             "Car Repair",
-            "Pay kr. 3.000 for repairing the car",
-            2);
+            "Pay kr. 3.000 for repairing the car", // 2 cards
+            6);
 
     CardTemplate newTires = new CardTemplate(
             "New Tires",
             "You have bought 4 new tires for your car, pay kr. 1.000",
-            2);
+            7);
 
     CardTemplate parkingFine = new CardTemplate(
             "Parking Fine",
             "You have received a parking fine, pay kr. 200 in fines",
-            2);
+            8);
 
     CardTemplate carInsurance = new CardTemplate(
             "Car Insurance",
             "Pay the car insurance, kr. 1.000",
-            2);
+            9);
 
     CardTemplate cigaretteTax = new CardTemplate(
             "Cigarette Tax",
             "You have been abroad and bought too many cigarettes, pay kr. 200 in tax",
-            2);
+            10);
 
     CardTemplate dentalBill = new CardTemplate(
             "Dental Bill",
             "Dental bill, pay kr. 2,000",
-            2);
+            11);
 
     CardTemplate lottery = new CardTemplate(
             "Lottery",
-            "You have won the class lottery. Receive Kr. 500 ",
-            2);
+            "You have won the class lottery. Receive Kr. 500 ", // 2 cards
+            12);
 
     CardTemplate dividend = new CardTemplate(
             "Dividend",
-            "You will receive your share dividend. Receive kr. 1.000 from the bank",
-            2);
+            "You will receive your share dividend. Receive kr. 1.000 from the bank", // 3 cards
+            13);
 
     CardTemplate governmentTax = new CardTemplate(
             "Government Tax",
             "The government has remitted a quarterly tax. Withdraw kr. 3.000 from the bank",
-            2);
+            14);
 
     CardTemplate jackPot = new CardTemplate(
             "Jackpot",
             "You have a row of eleven jackpot in tipping, receive kr.1000",
-            2);
+            15);
 
     CardTemplate salary = new CardTemplate(
             "Salary",
             "Due to the expensive time, you have received a salary increase, receive kr. 1.000.",
-            2);
+            16);
 
     CardTemplate growingBond = new CardTemplate(
             "GrowingBond",
-            "Your premium bond is growing. Receive 1000 kr. from the Bank",
-            2);
+            "Your premium bond is growing. Receive 1000 kr. from the Bank", // 2 cards
+            17);
 
     CardTemplate auction = new CardTemplate(
             "Auction",
             "They have sold some old furniture at auction. Receive kr. 1.000 from the bank",
-            2);
+            18);
 
     CardTemplate garden = new CardTemplate(
             "Garden",
-            "The value of own breeding from the utility garden is now kr. 200 which you receive from the bank",
-            2);
+            "The value of own breeding from the utility garden" +
+                    " is now kr. 200 which you receive from the bank",
+            19);
 
     CardTemplate scholarship = new CardTemplate(
             "Scholarship",
-            "You receive the Matador scholarship of kr. 40.000, but only if the values do not exceed Kr.15.000",
-            2);
+            "You receive the Matador scholarship of" +
+                    " kr. 40.000, but only if the values do not exceed Kr.15.000",
+            20);
 
     CardTemplate birthday = new CardTemplate(
             "Birthday",
             "Its your Birthday. Receipt of each teammate kr. 200",
-            2);
+            21);
 
     CardTemplate fellowship = new CardTemplate(
             "Fellowship",
-            "You have put money out for a fellowship feast. Oddly enough, everyone pays immediately. Receive kr. 500 from each teammate",
-            2);
+            "You have put money out for a fellowship feast." +
+                    "Oddly enough, everyone pays immediately. Receive kr. 500 from each teammate",
+            22);
 
     CardTemplate family_party = new CardTemplate(
             "Family Party",
             "You hold a family party and receive a subsidy from each partner of kr. 500",
-            2);
+            23);
 
     CardTemplate go_back = new CardTemplate(
             "Go Back",
-            "Move to start",
-            2);
+            "Move to START", // 2 cards
+            24);
 
     CardTemplate move_forward = new CardTemplate(
             "Move Forward",
             "Move three squares forward",
-            2);
+            25);
 
     CardTemplate move_back = new CardTemplate(
             "Move Back",
-            "Move three squares back",
-            2);
+            "Move three squares back", // 2 cards
+            26);
 
     CardTemplate frederiksberg_alle = new CardTemplate(
             "Frederiksberg Alle",
             "Move forward to Frederiksberg Alle. If you pas START, you will collect kr. 4000",
-            2);
+            27);
 
     CardTemplate twice_rent = new CardTemplate(
             "Twice Rent",
-            "Move to the nearest shipping company and pay the owner twice the rent he is otherwise entitled to",
-            2);
+            "Move to the nearest shipping company and pay the owner" +
+                    " twice the rent he is otherwise entitled to",
+            28);
 
     CardTemplate buy_company = new CardTemplate(
             "Buy Company",
-            "If the company is not owned by anyone, you can buy it from the bank",
-            2);
+            "If the company is not owned by anyone, you can buy it from the bank", // 2 cards
+            29);
 
     CardTemplate mols_line = new CardTemplate(
             "Mols Line",
             "Take the Mols Line, move the piece forward and if you pass START, collect kr. 4000.",
-            2);
+            30);
 
     CardTemplate grønningen = new CardTemplate(
             "Grønningen",
             "Move to Grønningen, if you pass the START, collect kr. 4000",
-            2);
+            31);
 
     CardTemplate vimmelskaftet = new CardTemplate(
             "Vimmelskaftet",
             "Move to Vimmelskaftet, if they pass START, collect kr. 4000",
-            2);
+            32);
 
     CardTemplate ferry = new CardTemplate(
             "Ferry",
             "Take the nearest ferry, if they pass the start collection then kr. 4.000",
-            2);
+            33);
 
     CardTemplate strandvejen = new CardTemplate(
             "Strandvejen",
             "Move forward to Strandvejen. If you pass START, you will collect kr 4.000.",
-            2);
+            34);
 
     CardTemplate town_hall_square = new CardTemplate(
             "Town Hall Square",
             "Go to the Town Hall Square",
-            2);
+            35);
 
     CardTemplate king_birthday = new CardTemplate(
             "King Birthday",
-            "On the occasion of the King's birthday, you are hereby pardoned for imprisonment. This card can keep until you need it or you can sell it",
-            2);
+            "On the occasion of the King's birthday, you are hereby pardoned for imprisonment." +
+                    " This card can keep until you need it or you can sell it", // 2 cards
+            36);
 
     CardTemplate jail = new CardTemplate(
             "Jail",
-            "Go to jail, you do not collect kr .4.000 to pass the start",
-            2);
+            "Go to jail, you do not collect kr .4.000 to pass the start", // 2 cards
+            37);
 
 
 
