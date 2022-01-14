05ba7210f6aca715507ed551dedb02c6baf97b75
Betim <s216250@student.dtu.dk>
2022-01-14 12:47:07 +0100

    CardTemplate index updated and number of cards

--- src/main/Test/java/dice/DieTest.java
+++ src/main/Test/java/dice/DieTest.java
@@ -4,14 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DieTest {
     @Test
-    public void testDieSide(){
-        int Sides = 6;
+    public void testDieMax_FaceValue(){
+        int Correct_Max_FaceValue = 6;
 
-        //Die object
-        Die die = new Die(Sides);
+        //Unit test: See if Correct_Max_FaceValue = Max face value
+        assertEquals(Correct_Max_FaceValue,Die.DIE_MAX_FACEVALUE);
+    }
 
-        //Unit test: See if Sides = Face value
-        assertEquals(die.getFaceValue(), Sides);
+    @Test
+    public void testDieMin_FaceValue(){
+        int Correct_Min_FaceValue = 1;
+
+        //Unit test: See if Correct_Min_FaceValue = Face value
+        assertEquals(Correct_Min_FaceValue, Die.FACE_VALUE);
+
     }
 
     }
